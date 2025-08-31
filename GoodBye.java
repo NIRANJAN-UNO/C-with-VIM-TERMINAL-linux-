81c2e8e5552de4f59754d12bfd322e0d83dc7565
NS <niranjseenu@gmail.com>
2025-08-31 10:10:00 +0530

    Initial commit- added first program

---
+++ GoodBye.java
@@ -0,0 +1,8 @@
+pubic class GoodBye {
+	public static void main(String[] args) {
+		System.out.println("Good bye");
+        System.out.print("hello world");
+        System.out.print("hello world");
+	}
+}
+
